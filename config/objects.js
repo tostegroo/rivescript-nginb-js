bb80e37cf48b0a5d5c0a7950d1f0a61df85faf91
Fabio Toste <tostegroo@gmail.com>
2017-04-03 11:47:27 -0300

    New object documentation

--- config/objects.js
+++ config/objects.js
@@ -1,6 +1,14 @@
 /**
+ * @global
+ * @mixin Objects
+ * @description Some custom objects to be used in NGINB
+*/
+
+/**
  * The user response options object
  * @member {Object} UserResponse
+ * @instance
+ * @memberof Objects
  * @property {Object|Boolean} response - An user object returned by mysql or cache or false if none
  * @property {String} data - The facebook data response
 */
@@ -8,6 +16,8 @@
 /**
  * The user update response options object
  * @member {Object} UserUpdateResponse
+ * @instance
+ * @memberof Objects
  * @property {Boolean} status - The status of the response
  * @property {String} data - The user data from the table | cache
 */
@@ -15,6 +25,8 @@
 /**
  * The bot instance object
  * @member {Object} Bot
+ * @instance
+ * @memberof Objects
  * @property {String} language - The language of the brain
  * @property {String} path - The path to the rivescript brain files
  * @property {Object} config - A config object to send to {@link https://github.com/aichaos/rivescript-js|rivescript}
@@ -24,6 +36,8 @@
 /**
  * The options object used for bot configuration
  * @member {Object} Options
+ * @instance
+ * @memberof Objects
  * @property {String} env - A string with an environment flag
  * @property {Bot[]} instances - Array object with bot instances
  * @property {String} assets_url - The path to assets to be used as attachment,
@@ -42,6 +56,8 @@
 /**
  * The bot options object
  * @member {Object} BotConfig
+ * @instance
+ * @memberof Objects
  * @property {Boolean} use_permanent_bot_user - A flag to tell the bot if it can use permanent user save or just cache
  * @property {Boolean} typing_delay - A flag to tell the bot if has any delay in bot response
  * @property {Number} typing_time - The time used for calculate the delay time in typing (characters per second)
@@ -54,6 +70,8 @@
 /**
  * The database options object
  * @member {Object} DatabaseConfig
+ * @instance
+ * @memberof Objects
  * @property {Object} config - A Database config used by {@link https://github.com/martinj/node-mysql-promise|mysql-promise}
  * @property {String} user_table - The name of user table in your database if any
  * @property {String} pages_table - The name of pages table in your database if any
@@ -63,6 +81,8 @@
 /**
  * The dispatcher options object
  * @member {Object} DispatcherConfig
+ * @instance
+ * @memberof Objects
  * @property {String} time - time flag to be used in {@link https://github.com/kelektiv/node-cron|node-cron}
  * @property {Function} function - The function to dispatch with cron at given time
 */
@@ -70,6 +90,8 @@
 /**
  * The facebook options object
  * @member {Object} FacebookConfig
+ * @instance
+ * @memberof Objects
  * @property {Boolean} send_to - A flag to tell the messenger controller to send responses to facebook
  * @property {String} graph_url - The facebook graph API url
  * @property {String} version - The facebook graph API version
@@ -82,6 +104,8 @@
 /**
  * The facebook page object - This object is created with a string as key (page id) and with an object with properties below - Eg: {"123456789789787": {}}
  * @member {Object} PageObjectConfig
+ * @instance
+ * @memberof Objects
  * @property {String} language - The language of the page
  * @property {String} type - The environment flag of the page
  * @property {String} appID - The facebook APP ID used for this bot
@@ -91,6 +115,8 @@
 /**
  * The page object used by the bot to identify the facebook page
  * @member {Object} PageConfig - A facebook config page object
+ * @instance
+ * @memberof Objects
  * @property {String} id - The id of the page
  * @property {String} token - The token of the page
  * @property {String} language - The language of the page (Optional)
@@ -100,6 +126,8 @@
 /**
  * The event object used by the bot
  * @member {Object} Event - Object sent by facebook controller
+ * @instance
+ * @memberof Objects
  * @property {PageConfig} fb_page - A page object
  * @property {String} sender - The facebook user pid
  * @property {String} type - The type of the message (message|payload|attachment)
@@ -111,6 +139,8 @@
 /**
  * The word object used by entities
  * @member {Object} WordConfig - Object of entities used for subs in rivescript
+ * @instance
+ * @memberof Objects
  * @property {String} value - The base word
  * @property {Array} synonyms - The word array with the synonyms
  */
@@ -118,6 +148,8 @@
 /**
  * The entity object used by the bot
  * @member {Object} EntityConfig - Object of entities used for subs in rivescript
+ * @instance
+ * @memberof Objects
  * @property {String} name - The name of the entity
  * @property {WordConfig[]} entries - The array of words and it's synonyms
  */
